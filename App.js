08cefdc45dbfd6013ac76880754084f3f92e0d5f
Jun Lee <dan.junwoo.lee@gmail.com>
2024-04-10 17:03:23 -0400

    handling origin

--- App.js
+++ App.js
@@ -12,11 +12,28 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
+const FRONTEND_BASE = process.env.FRONTEND_URL;
+
+// code credit goes to Piazza post @1134
+const VALID_BRANCHES = ["main", "a5", "a6"]
+const allowedOrigins = [FRONTEND_BASE, ...VALID_BRANCHES.map((branch) => `https://${branch}--${FRONTEND_BASE.replace("https://", "")}`)]
+
+const handle_origin = (requestOrigin, callback) => {
+  if (!requestOrigin || allowedOrigins.includes(requestOrigin)) {
+    return callback(null, true);
+  } else {
+    const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+    return callback(new Error(msg), false);
+  }
+}
+
+const HTTP_SERVER_DOMAIN_BASE = process.env.HTTP_SERVER_DOMAIN;
+
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: handle_origin,
   })
 );
 const sessionOptions = {
@@ -31,6 +48,7 @@ if (process.env.NODE_ENV !== "development") {
     secure: true,
   };
 }
+
 app.use(session(sessionOptions));
 app.use(express.json());
 CourseRoutes(app);
